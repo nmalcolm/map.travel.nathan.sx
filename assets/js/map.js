5f5418f029dfdbf2e12f7e6bfb5f6bd24bb3548f
Nathan <nathan@Ns-MacBook-Pro.local>
2018-12-26 15:39:02 +0000

    New website

---
+++ assets/js/map.js
@@ -0,0 +1,28 @@
+$(function() {
+  var visited = "#2ecc71";
+  var home = "#3498db";
+
+  var data = {
+    "GB": home,
+    "UA": visited
+  };
+
+  $('#vmap').vectorMap({
+    map: 'world_mill',
+    backgroundColor: '#0f2744',
+    series: {
+      regions: [{
+        values: data,
+        attribute: 'fill'
+      }]
+    }, onRegionTipShow: function(e, label, code) {
+      if (code == "GB") {
+        label.html('<div class="map-tooltip">' + label.html() + ' is my home country.</div>');
+      } else if (code in data) {
+        label.html('<div class="map-tooltip">I\'ve visited ' + label.html() + '!</div>');
+      } else {
+        label.html('<div class="map-tooltip">' + label.html() + ': I haven\'t visited this country yet!</div>');
+      }
+    }
+  });
+});
